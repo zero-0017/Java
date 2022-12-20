e96f012d136ff3ad4c7a708c39e787dccae8f2bf
zero-0017 <hiromichi.v.0017@gmail.com>
2022-12-20 07:31:06 +0000

    [Java]4章

---
+++ wcp/Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        final int LUCKY_NUMBER = 17;
+        String str = "植木広通";
+        System.out.println(str);
+        str = "やらなくてもいいことならやらない。やらなければいけないことなら手短に。";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
