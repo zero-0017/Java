9e1123a716e65520fbcd0cd3c92bdd208e8ee018
zero-0017 <hiromichi.v.0017@gmail.com>
2022-12-20 06:30:00 +0000

    [Java]3章

--- wcp/Chapter02.java
+++ wcp/Chapter02.java
@@ -1,5 +1,6 @@
 public class Chapter02 {
     public static void main(String[] args) {
-        System.out.println("こんにちは！");
+        System.out.println("Hello WEBCAMP!");
+        System.out.println("Hello Java!");
     }
 }
